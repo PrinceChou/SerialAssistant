1744e5d33706756e48081303d86d543bcba5d57f
Peach <568797992@qq.com>
2019-06-19 09:44:09 +0800

    WPF Test

---
+++ WPFSerialAssistant/LoginEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFSerialAssistant
+{
+    class LoginEvent
+    {
+    }
+
+    public partial class Login : Window
+    {
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WPFSerialAssistant.About about = new About();
+            about.ShowDialog();
+        }
+    }
+}
